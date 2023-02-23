5657424e4b99f268a334f8c972cf942106906ffa
Noisyboy120 <94278518+Noisyboy120@users.noreply.github.com>
2023-02-23 16:44:52 -0600

    added sword

---
+++ Assets/WeaponController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject IceSword;
+    public bool CanAttack = true;
+    public float AttackCoolDown = 1.0f;
+
+    //Making audio
+    public AudioClip SwordAttackSound;
+
+    void Update() 
+    {
+        //Checks if leftClick is pressed down
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            if(CanAttack == true)
+            {
+                SwordAttack();
+            }
+        }
+    }
+
+    public void SwordAttack() 
+    {
+        CanAttack = false;
+        Animator anim = IceSword.GetComponent<Animator>();
+        anim.SetTrigger("Attack");
+
+        //sound
+        AudioSource ac = GetComponent<AudioSource>();
+        ac.PlayOneShot(SwordAttackSound);
+
+
+        StartCoroutine(ResetAttackCooldown());
+    }
+
+    IEnumerator ResetAttackCooldown()
+    {
+        yield return new WaitForSeconds(AttackCoolDown);
+        CanAttack = true;
+    }
+}
